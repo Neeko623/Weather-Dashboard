69f16fb549c3fe483ec43544203346fbd30e7c63
Neeko Tang <neeko_tvxq@hotmail.com>
2021-01-17 14:38:11 -0500

    update localstorage

--- script.js
+++ script.js
@@ -24,21 +24,33 @@ function getCoordAPI(cityName) {
         })
         .then(function (data) {
           drawWeather(data); //link function to get current weather
-          foreCast(data); // foreCast(data); //link function to get future weather
+          foreCast(data); //link function to get future weather
         });
     });
 }
 
 // onclick function to retrive current city result
 var searchBtn = document.getElementById("search-button");
-var searchInput = document.getElementById("city");
+var userInput = document.getElementById("city");
 
 searchBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  var cityName = searchInput.value.trim();
+  var cityName = userInput.value.trim();
   if (cityName) {
     getCoordAPI(cityName);
-    searchInput.value = "";
+    userInput.value = "";
+    //add to storage
+    cityList = JSON.parse(localStorage.getItem("city_list"));
+    if (!cityList || cityList.length == 0) {
+      cityList = [cityName]
+      localStorage.setItem("city_list",JSON.stringify(cityList));
+    }
+    else {
+      if (cityList.indexOf(cityName) == -1) {
+          cityList.push(cityName)
+      }
+      localStorage.setItem("city_list",JSON.stringify(cityList));
+    }
   } else {
     alert("Invaild Input");
   }
@@ -47,56 +59,48 @@ searchBtn.addEventListener("click", function (event) {
 // Display the current weather after grabing the city name form the input text box.
 function drawWeather(response) {
   var cur = response.current;
-  console.log(response);
-  // //set up new date format
-  // var date=new Date(daily[i].dt*1000).toLocaleDateString();
   // convert to celcius
   var celcius = Math.round(parseFloat(cur.temp) - 273.15);
+  document.getElementById("temp").innerHTML = "Temperature: " + celcius + "°C";
   // convert to MPH
   var ws = cur.wind_speed;
   var windsmph = (ws * 2.237).toFixed(1);
+  document.getElementById("wind-speed").innerHTML =
+    "Wind-speed: " + windsmph + "MPH";
   // display weather icon image
   var weatherIcon = cur.weather[0].icon + ".png";
   iconImg = "https://openweathermap.org/img/w/" + weatherIcon;
-  console.log(iconImg);
   document.getElementById("icon").src = iconImg;
-
-  document.getElementById("temp").innerHTML = "Temperature: " + celcius + "°C";
+  // display weather humidity
   document.getElementById("humidity").innerHTML =
     "Humidity: " + cur.humidity + "%";
-  document.getElementById("wind-speed").innerHTML =
-    "Wind-speed: " + windsmph + "MPH";
+   // display weather uv-index
   document.getElementById("uv-index").innerHTML = cur.uvi;
 }
 
 // Display the following 5 day forecast weather
-
 function foreCast(resp) {
-  var date = new Date()
   var daily = resp.daily;
   daily.map((itm, i) => {
     if (i < 5) {
       // get forecast dates
-      var foreCastDates = daily[i].dt;
-      document.getElementById("fDate" + i).innerHTML = foreCastDates;
-      console.log(foreCastDates);
+      var dateFormat = new Date (daily[i].dt * 1000).toLocaleDateString("en-US");
+      document.getElementById("fDate" + i).innerHTML = dateFormat;
 
       // get forecast icon images
       var foreCastIcon = daily[i].weather[0].icon + ".png";
       foreCastIconImg =
         "https://openweathermap.org/img/w/" + foreCastIcon;
       document.getElementById("fImg" + i).src = foreCastIconImg;
-      console.log(foreCastIconImg);
 
       // get forecast temperature
-      var foreCastTemp = daily[i].temp;
-      document.getElementById("fTemp" + i).innerHTML = foreCastTemp;
-      console.log(foreCastTemp);
+      var foreCastTemp = Math.round(parseFloat(daily[i].temp.max) - 273.15);
+      document.getElementById("fTemp" + i).innerHTML = foreCastTemp + "°C";
 
       // get forecast humidity
       var foreCastHumidity = daily[i].humidity;
-      document.getElementById("fHumidity"+ i).innerHTML = foreCastHumidity;
-      console.log(foreCastHumidity);
+      document.getElementById("fHumidity"+ i).innerHTML = foreCastHumidity + "%";
+
     }
   });
 }
